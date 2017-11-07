bc193c65a125642d59e49bbbd25b6fd7103a57ab
Jessi810 <jessisibayan@gmail.com>
2017-11-07 16:04:00 +0800

    Added seed data

--- PICECAR/Migrations/ApplicationDbContext/Config01.cs
+++ PICECAR/Migrations/ApplicationDbContext/Config01.cs
@@ -1,5 +1,8 @@
 namespace PICECAR.Migrations.ApplicationDbContext
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -16,18 +19,66 @@ namespace PICECAR.Migrations.ApplicationDbContext
 
         protected override void Seed(PICECAR.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            string[] roles =
+            {
+                "Administrator",
+                "Secretary",
+                "User"
+            };
+            RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
+            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
+            IdentityRole identityRole;
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // Add a default roles
+            foreach (string role in roles)
+            {
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    identityRole = new IdentityRole { Name = role };
+
+                    roleManager.Create(identityRole);
+                }
+            }
+
+            // Adds a default account
+            string[,] users =
+            {
+                { "admin@email.com", "Admin@123", "Administrator" },
+                { "secretary@email.com", "Admin@123", "Secretary" },
+                { "user@email.com", "Admin@123", "User" }
+            };
+            string email;
+
+            for (var i = 0; i < users.Length / 3; i++)
+            {
+                email = users[i, 0];
+
+                // Add the account if it does not exist
+                if (!context.Users.Any(u => u.Email == email))
+                {
+                    var store = new UserStore<ApplicationUser>(context);
+                    var manager = new UserManager<ApplicationUser>(store);
+                    var user = new ApplicationUser
+                    {
+                        Email = email,
+                        UserName = email
+                    };
+
+                    manager.Create(user, users[i, 1]);
+                    manager.AddToRole(user.Id, users[i, 2]);
+
+                    var pInfo = new PersonalInfo
+                    {
+                        Id = user.Id,
+                        FirstName = "First",
+                        MiddleName = "Middle",
+                        LastName = "Last"
+                    };
+
+                    context.PersonalInfos.AddOrUpdate(pInfo);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
