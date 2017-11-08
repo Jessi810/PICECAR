3a9f4a141cdb495265d40eeee0df0d2779832ba3
Jessi810 <jessisibayan@gmail.com>
2017-11-08 14:35:12 +0800

    Added transact controller

---
+++ PICECAR/Controllers/TransactController.cs
@@ -0,0 +1,39 @@
+﻿using PICECAR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PICECAR.Controllers
+{
+    [Authorize]
+    public class TransactController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Transact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Seminar")]
+        public ActionResult AddSeminar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Seminar")]
+        public ActionResult AddSeminar(Transact.Seminar model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
